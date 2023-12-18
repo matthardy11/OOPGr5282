539b366aa5ba32bf85a33fc6a5e50fd9a4557ed9
matthardy11 <matthardy2560@gmail.com>
2023-12-18 22:05:09 +0400

    add fields, constructor, getter, setter, toString  in class Product

--- OOPSeminar1/VendingMachine/src/domen/Product.java
+++ OOPSeminar1/VendingMachine/src/domen/Product.java
@@ -1,4 +1,56 @@
 package domen;
 
 public class Product {
+    private String name;
+    private int place;
+    private int price;
+    private String type;
+
+    public Product(String name, int place, int price, String type) {
+        this.name = name;
+        this.place = place;
+        this.price = price;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPlace() {
+        return place;
+    }
+
+    public void setPlace(int place) {
+        this.place = place;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+   @Override
+
+   public String toString(){
+        return "Product " + "name = " + this.name
+                            + "place = " + this.place
+                            + "price = " + this.price
+                            + "type = " + this.type;
+   }
 }
