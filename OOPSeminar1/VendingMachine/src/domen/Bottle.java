a2814aeb2b292a48ad3ce1c4e82f8c97c19f97e3
matthardy11 <matthardy2560@gmail.com>
2023-12-18 22:39:53 +0400

    add Bottle class

---
+++ OOPSeminar1/VendingMachine/src/domen/Bottle.java
@@ -0,0 +1,18 @@
+package domen;
+
+public class Bottle extends Product{
+    private double volume;
+
+    public Bottle(String name, int place, int price, String type, double volume) {
+        super(name, place, price, type);
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+}
