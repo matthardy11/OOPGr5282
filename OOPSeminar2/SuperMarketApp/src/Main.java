d9718f7431cbe4f75b6e5a5941828da25403ec49
matthardy11 <matthardy2560@gmail.com>
2024-01-12 16:33:20 +0400

    first run project

--- OOPSeminar2/SuperMarketApp/src/Main.java
+++ OOPSeminar2/SuperMarketApp/src/Main.java
@@ -1,5 +1,21 @@
+import Classes.Actor;
+import Classes.Market;
+import Classes.OrdinaryClient;
+import Classes.SpecialClient;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Market magnit = new Market();
+
+        Actor client1 = new OrdinaryClient("Boris");
+        Actor client2 = new OrdinaryClient("Maria");
+        Actor client3 = new SpecialClient("Gubernator", 1);
+
+        magnit.acceptToMarket(client1);
+        magnit.acceptToMarket(client2);
+        magnit.acceptToMarket(client3);
+
+        magnit.update();
     }
+
 }
